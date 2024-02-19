77a7390cbcd7a0a1180e6d032992c5b59ba34efd
SohelDewan <sohelds241@gmail.com>
2024-02-19 11:23:01 +0600

    javaScript initial step add

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const btn = document.getElementsByClassName('seat-btn');
-console.log(btn);
+const allBtn = document.getElementsByClassName("seat-btn");
+// console.log(allBtn);
+
+let count = 0;
+let seatLeft = 40;
+for (let btn of allBtn) {
+  // console.log(btn);
+  btn.addEventListener("click", (e) => {
+    count = count + 1;
+    seatLeft = seatLeft - 1;
+    document.getElementById('seat-count').innerText = count;
+    document.getElementById('seat-left').innerText = seatLeft;
+  });
+}
